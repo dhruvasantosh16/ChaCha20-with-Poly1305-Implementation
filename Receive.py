68f7637b63ddc4e023320093d2999e76c5d509d6
Dhruva Santosh <dhruvasantosh16@outlook.com>
2021-04-11 16:17:38 +0530

    numba commit

--- Receive.py
+++ Receive.py
@@ -1,9 +1,11 @@
 from Cryptodome.Cipher import ChaCha20_Poly1305
+from numba import jit
 import socket
 import time
 
 start_time = time.time()
 
+@jit(nopython=True)
 def sockreceive():
     host = '127.0.0.1'
     port = 12345
@@ -12,7 +14,7 @@ def sockreceive():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
         s1.bind((host, port))
         s1.listen()
-        c1, addr1 = s1.accept()
+        c1= s1.accept()
         with c1:
             data1 = c1.recv(2000)
             c1.sendall(b'acknowledged')
@@ -22,7 +24,7 @@ def sockreceive():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
         s2.bind((host, port1))
         s2.listen()
-        c2, addr2 = s2.accept()
+        c2= s2.accept()
         with c2:
             data2 = c2.recv(2000)
             c2.sendall(b'acknowledged')
@@ -32,7 +34,7 @@ def sockreceive():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s3:
         s3.bind((host, port2))
         s3.listen()
-        c3, addr3 = s3.accept()
+        c3= s3.accept()
         with c3:
             data3 = c3.recv(2000)
             c3.sendall(b'acknowledged')
@@ -40,6 +42,7 @@ def sockreceive():
         s3.close()
     return data1, data2, data3
 
+@jit(nopython=True)
 def dec(data1, data2, data3):
     key = b'\x1a\xe0J\xb7\xfe\x18\x08>+\xd9\xb4\xb8,+n#\xef\xc1\x0b\xa2\xa3\x01\x8c\xf4\xd7\x17\xbf\xc9\xc0\x0c\xb0z'
     cipher = ChaCha20_Poly1305.new(key=key, nonce=data3)
@@ -48,6 +51,7 @@ def dec(data1, data2, data3):
     plaintext = int.from_bytes(plaintext_b, byteorder="little",signed=0)
     print(plaintext)
 
+@jit(nopython=True)
 def main():
     data1, data2, data3 = sockreceive()
     dec(data1, data2, data3)
--- Transmission.py
+++ Transmission.py
@@ -1,6 +1,7 @@
 from Cryptodome.Cipher import ChaCha20_Poly1305
 from Cryptodome.Random import get_random_bytes
 from Cryptodome.Random.random import randint  
+from numba import jit
 import time
 import socket
 
@@ -8,6 +9,7 @@ import socket
 time_start = time.time()
 
 #Input Generation
+@jit(nopython=True)
 def input():
     inp = randint(0,1023)
     print("Randomized Input: ")
@@ -16,6 +18,7 @@ def input():
     return inp_b
 
 #Ciphertext Generation
+@jit(nopython=True)
 def gen(inp_b):
     key = b'\x1a\xe0J\xb7\xfe\x18\x08>+\xd9\xb4\xb8,+n#\xef\xc1\x0b\xa2\xa3\x01\x8c\xf4\xd7\x17\xbf\xc9\xc0\x0c\xb0z'
     cipher = ChaCha20_Poly1305.new(key=key)
@@ -24,6 +27,7 @@ def gen(inp_b):
     return ciphertext, tag, nnc
 
 #socket variables
+@jit(nopython=True)
 def socksend(ciphertext, tag, nnc):
     host = '127.0.0.1'
     port = 12345
@@ -57,6 +61,7 @@ def socksend(ciphertext, tag, nnc):
         print(data3)
     s3.close()
 
+@jit(nopython=True)
 def main():
     inp_b = input()
     ciphertext, tag, nnc = gen(inp_b)
