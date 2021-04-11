c4fb3e07327024e94478eb4a9f7dbec86579e22d
Dhruva Santosh <dhruvasantosh16@outlook.com>
2021-04-11 17:49:01 +0530

    new additions

--- Receive.py
+++ Receive.py
@@ -49,9 +49,58 @@ def dec(data1, data2, data3):
     plaintext = int.from_bytes(plaintext_b, byteorder="little",signed=0)
     print(plaintext)
 
+def calc(plaintext):
+    plaintext = plaintext ** (1/3)
+    angle = plaintext*10
+    print("Angle of Servo: ",angle)
+    angle_b = angle.to_bytes(length=2, byteorder="little", signed=0)
+
+def enc(angle):
+    key = b'\x1a\xe0J\xb7\xfe\x18\x08>+\xd9\xb4\xb8,+n#\xef\xc1\x0b\xa2\xa3\x01\x8c\xf4\xd7\x17\xbf\xc9\xc0\x0c\xb0z'
+    cipher = ChaCha20_Poly1305.new(key=key)
+    ciphertext_r, tag_r = cipher.encrypt_and_digest(angle_b)
+    nnc_r = cipher.nonce
+    return ciphertext_r, tag_r, nnc_r
+
+def socksend(ciphertext_r, tag_r, nnc_r):
+    host = '127.0.0.1'
+    port = 12345
+    port1 = 12346
+    port2 = 12347
+    print(ciphertext_r)
+    print(tag_r)
+    print(nnc_r)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
+        s1.connect((host,port))
+        print('sending ciphertext...')
+        s1.sendall(ciphertext_r)
+        data1 = s1.recv(1024)
+        print(data1)
+    s1.close()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
+        s2.connect((host,port1))
+        print('sending tag...')
+        s2.sendall(tag_r)
+        data2 = s2.recv(1024)
+        print(data2)
+    s2.close()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s3:
+        s3.connect((host,port2))
+        print('sending nnc...')
+        s3.sendall(nnc_r)
+        data3 = s3.recv(1024)
+        print(data3)
+    s3.close()
+
 def main():
     data1, data2, data3 = sockreceive()
     dec(data1, data2, data3)
+    calc(plaintext)
+    ciphertext_r, tag_r, nnc_r = enc(angle_b)
+    socksend(ciphertext_r, tag_r, nnc_r)
 
 if __name__ == "__main__":
     main()
