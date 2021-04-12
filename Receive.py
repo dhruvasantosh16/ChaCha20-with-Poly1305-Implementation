f34e8264528ffef72034d3488395fe6bb0dc9617
Barebones00 <nikhilgooty@gmail.com>
2021-04-12 18:00:52 +0530

    optimised socket function

--- Receive.py
+++ Receive.py
@@ -1,45 +1,68 @@
 from Cryptodome.Cipher import ChaCha20_Poly1305
-from numba import jit
+#from numba import jit
 import socket
 import time
+import math
 
 start_time = time.time()
 
-def sockreceive():
+def sock(s, *args):
     host = '127.0.0.1'
     port = 12345
     port1 = 12346
     port2 = 12347
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
-        s1.bind((host, port))
-        s1.listen()
-        c1= s1.accept()
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s1.bind((host,port))
+    s2.bind((host,port1))
+    s3.bind((host,port2))
+    s1.listen()
+    s2.listen()
+    s3.listen()
+    c1, addr1 = s1.accept()
+    c2, addr2 = s2.accept()
+    c3, addr3 = s3.accept()
+    if s == "send":
+        data1 = args[0]
+        data2 = args[1]
+        data3 = args[2]
         with c1:
-            data1 = c1.recv(2000)
-            c1.sendall(b'acknowledged')
-            print('received data', data1)
-        s1.close()
+            print("sending", data1)
+            c1.sendall(data1)
+            ack = c1.recv(1024)
+            print(ack)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
-        s2.bind((host, port1))
-        s2.listen()
-        c2= s2.accept()
+        
         with c2:
-            data2 = c2.recv(2000)
-            c2.sendall(b'acknowledged')
-            print('received data', data2)
-        s2.close()
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s3:
-        s3.bind((host, port2))
-        s3.listen()
-        c3= s3.accept()
+            print("sending", data2)
+            c2.sendall(data2)
+            ack2 = c2.recv(1024)
+            print(ack2)
+        
+        
         with c3:
-            data3 = c3.recv(2000)
-            c3.sendall(b'acknowledged')
-            print('received data', data3)
-        s3.close()
-    return data1, data2, data3
+            print("sending", data3)
+            c3.sendall(data3)
+            ack3 = c3.recv(1024)
+            print(ack3)
+
+    else:
+        with c1:
+            rdata1 = c1.recv(2000)
+            print('received', rdata1)
+            c1.sendall(b"acknowledged")
+
+        with c2:
+            rdata2 = c2.recv(2000)
+            print('received', rdata2)
+            c2.sendall(b"acknowledged")
+
+        with c3:
+            rdata3 = c3.recv(2000)
+            print('received', rdata3)
+            c3.sendall(b"acknowledged")
+            return rdata1, rdata2, rdata3
  
 def dec(data1, data2, data3):
     key = b'\x1a\xe0J\xb7\xfe\x18\x08>+\xd9\xb4\xb8,+n#\xef\xc1\x0b\xa2\xa3\x01\x8c\xf4\xd7\x17\xbf\xc9\xc0\x0c\xb0z'
@@ -48,59 +71,28 @@ def dec(data1, data2, data3):
     cipher.verify(data2)
     plaintext = int.from_bytes(plaintext_b, byteorder="little",signed=0)
     print(plaintext)
+    return plaintext
 
 def calc(plaintext):
     plaintext = plaintext ** (1/3)
-    angle = plaintext*10
+    angle = math.floor(plaintext*10)
     print("Angle of Servo: ",angle)
     angle_b = angle.to_bytes(length=2, byteorder="little", signed=0)
+    return angle_b
+    
 
-def enc(angle):
+def enc(angle_b):
     key = b'\x1a\xe0J\xb7\xfe\x18\x08>+\xd9\xb4\xb8,+n#\xef\xc1\x0b\xa2\xa3\x01\x8c\xf4\xd7\x17\xbf\xc9\xc0\x0c\xb0z'
     cipher = ChaCha20_Poly1305.new(key=key)
     ciphertext_r, tag_r = cipher.encrypt_and_digest(angle_b)
     nnc_r = cipher.nonce
     return ciphertext_r, tag_r, nnc_r
 
-def socksend(ciphertext_r, tag_r, nnc_r):
-    host = '127.0.0.1'
-    port = 12345
-    port1 = 12346
-    port2 = 12347
-    print(ciphertext_r)
-    print(tag_r)
-    print(nnc_r)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
-        s1.connect((host,port))
-        print('sending ciphertext...')
-        s1.sendall(ciphertext_r)
-        data1 = s1.recv(1024)
-        print(data1)
-    s1.close()
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
-        s2.connect((host,port1))
-        print('sending tag...')
-        s2.sendall(tag_r)
-        data2 = s2.recv(1024)
-        print(data2)
-    s2.close()
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s3:
-        s3.connect((host,port2))
-        print('sending nnc...')
-        s3.sendall(nnc_r)
-        data3 = s3.recv(1024)
-        print(data3)
-    s3.close()
-
 def main():
-    data1, data2, data3 = sockreceive()
-    dec(data1, data2, data3)
-    calc(plaintext)
+    data1, data2, data3 = sock("r")
+    plaintext = dec(data1, data2, data3)
+    angle_b = calc(plaintext)
     ciphertext_r, tag_r, nnc_r = enc(angle_b)
-    socksend(ciphertext_r, tag_r, nnc_r)
 
 if __name__ == "__main__":
     main()
--- Transmission.py
+++ Transmission.py
@@ -1,7 +1,7 @@
 from Cryptodome.Cipher import ChaCha20_Poly1305
 from Cryptodome.Random import get_random_bytes
 from Cryptodome.Random.random import randint  
-from numba import jit
+#from numba import jit
 import time
 import socket
 
@@ -25,48 +25,57 @@ def gen(inp_b):
     ciphertext, tag = cipher.encrypt_and_digest(inp_b)
     nnc = cipher.nonce
     return ciphertext, tag, nnc
-
-#socket variables
   
-def socksend(ciphertext, tag, nnc):
+def sock(s, *args):
     host = '127.0.0.1'
     port = 12345
     port1 = 12346
     port2 = 12347
-    print(ciphertext)
-    print(tag)
-    print(nnc)
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s1.connect((host,port))
+    s2.connect((host,port1))
+    s3.connect((host,port2))
+    
+    if s == "send":
+        data1 = args[0]
+        data2 = args[1]
+        data3 = args[2]
+        print("sending", data1)
+        s1.sendall(data1)
+        ack = s1.recv(2000)
+        print(ack)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
-        s1.connect((host,port))
-        print('sending ciphertext...')
-        s1.sendall(ciphertext)
-        data1 = s1.recv(1024)
-        print(data1)
-    s1.close()
+        print("sending", data2)
+        s2.sendall(data2)
+        ack = s2.recv(2000)
+        print(ack)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
-        s2.connect((host,port1))
-        print('sending tag...')
-        s2.sendall(tag)
-        data2 = s2.recv(1024)
-        print(data2)
-    s2.close()
+        print("sending", data3)
+        s3.sendall(data3)
+        ack = s3.recv(2000)
+        print(ack)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s3:
-        s3.connect((host,port2))
-        print('sending nnc...')
-        s3.sendall(nnc)
-        data3 = s3.recv(1024)
-        print(data3)
-    s3.close()
+    else:
+        rdata1 = s1.recv(2000)
+        print("recieved", rdata1)
+        s1.sendall(b"acknowledged")
+        
+        rdata2 = s2.recv(2000)
+        print("received", rdata2)
+        s2.sendall(b"acknowledged")
 
-  
+        rdata3 = s3.recv(2000)
+        print("received", rdata3)
+        s3.sendall(b"acknowledged")
+        return rdata1, rdata2, rdata3
+
 def main():
     inp_b = input()
     ciphertext, tag, nnc = gen(inp_b)
-    socksend(ciphertext, tag, nnc)
-
+    sock("send",ciphertext, tag, nnc)
+    
 if __name__ == "__main__":
     main()
   
